5981babcf5a4566b676c58affeb2439279edb6a3
Avrelii <lvgglpl@gmail.com>
2026-08-12 06:38:53 +0300

    fix: drop duplicate index.ts that broke typecheck (JSX in a .ts file)

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App';
-import { AuthGate } from './src/auth/AuthGate';
-
-function Root() {
-  return <AuthGate><App /></AuthGate>;
-}
-
-registerRootComponent(Root);
